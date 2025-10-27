6ab345a87502c153f22424b4315f5a78e7a083f0
Leshan <leshanpasindu12@gmail.com>
2025-10-27 06:24:10 +0530

    basic react app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Contact from "./pages/Contact"
+
 
 function App() {
 
   return (
     <>
-      
+      <Router>
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
+          </ul>
+        </nav>
+
+
+        <Routes>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="/about" element={<About/>}></Route>
+          <Route path="/contact" element={<Contact/>}></Route>
+        </Routes>
+      </Router>
     </>
   )
 }

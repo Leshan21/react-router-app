f0e8a2e0a19badf393e9e72cccddfce2211ec127
Leshan <leshanpasindu12@gmail.com>
2025-11-01 09:23:15 +0530

    basic setub for start the project

--- src/App.jsx
+++ src/App.jsx
@@ -8,23 +8,8 @@ function App() {
 
   return (
     <>
-      <Router>
-        <nav>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">About</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
-          </ul>
-        </nav>
-
-
-        <Routes>
-          <Route path="/" element={<Home/>}></Route>
-          <Route path="/about" element={<About/>}></Route>
-          <Route path="/contact" element={<Contact/>}></Route>
-        </Routes>
-      </Router>
-    </>
+      
+    </>  
   )
 }
 

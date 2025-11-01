13e5c96af5ef55204769be719e564701c783f69b
Leshan <leshanpasindu12@gmail.com>
2025-11-01 09:31:36 +0530

    add nav bar to home page

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,24 @@ import ProtectedRoute from "./components/ProtectedRoute"
 import { useState } from "react"
 
 function App() {
-
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <>
+      <div>
+        <h1>My Router App </h1>
 
+        <nav>
+          <Link to="/">Home</Link> | {" "}
+          <Link to="/about">About</Link> | {" "}
+          <Link to="/users">Users</Link> | {" "}
+          <Link to="/dashboard">DashBoard</Link> | {" "}
+          <Link to="/login">Login</Link>
+        </nav>
+
+        <hr />
+
+        
+      </div>
     </>  
   )
 }

6d42867aa048b9317c0959f6fd12759c1c18cd2d
Leshan <leshanpasindu12@gmail.com>
2025-11-01 09:26:32 +0530

    add pages , protectedroute and useState to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
-import Contact from "./pages/Contact"
-
+import Users from "./pages/Users"
+import UserDetail from "./pages/UserDetails"
+import Dashboard from "./pages/Dashboard"
+import Login from "./pages/Login"
+import ProtectedRoute from "./components/ProtectedRoute"
+import { useState } from "react"
 
 function App() {
 
   return (
     <>
-      
+
     </>  
   )
 }

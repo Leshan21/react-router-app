e4112845bc0c0d9371a184ddeb06f38272abf9ae
Leshan <leshanpasindu12@gmail.com>
2025-11-01 17:05:28 +0530

    simple route

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,25 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
-import Home from "./pages/Home"
-import About from "./pages/About"
-import Users from "./pages/Users"
-import UserDetail from "./pages/UserDetails"
-import Dashboard from "./pages/Dashboard"
-import Login from "./pages/Login"
-import ProtectedRoute from "./components/ProtectedRoute"
-import { useState } from "react"
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+// import Users from "./pages/Users";
+// import UserDetail from "./pages/UserDetails";
+// import Dashboard from "./pages/Dashboard";
+// import Login from "./pages/Login";
+// import ProtectedRoute from "./components/ProtectedRoute";
+import { useState } from "react";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <>
-      <div>
-        <h1>My Router App </h1>
-
-        <nav>
-          <Link to="/">Home</Link> | {" "}
-          <Link to="/about">About</Link> | {" "}
-          <Link to="/users">Users</Link> | {" "}
-          <Link to="/dashboard">DashBoard</Link> | {" "}
-          <Link to="/login">Login</Link>
-        </nav>
-
-        <hr />
-
-        <Routes>
-          <Route path="/" element={<Home/>}></Route>
-          <Route path="/about" element={<About/>}></Route>
-          <Route path="/users" element={<Users/>}></Route>
-          <Route path="/users/:id" element={<UserDetail/>}></Route>
-        </Routes>
-      </div>
-    </>  
-  )
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/about" element={<About/>}></Route>
+      </Routes>
+      
+      
+    </>
+  );
 }
 
-export default App
+export default App;
